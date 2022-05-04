830f870c2cdcc64eb52f371d0c5d3ba5c4e39fe2
annswob <annaswobodaa@gmail.com>
2022-05-04 20:40:02 +0200

    commit2

---
+++ Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/01_Application Quickstart Guide/01_1 Download and install the application.htm'],s:['+0'],p:[['+0',5],['owner',4]]});
---
+++ Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=637871112822282793'
+});
