e9b780546c24a1e62de42b22e602b9d5916689ed
annswob <annaswobodaa@gmail.com>
2022-05-04 20:33:20 +0200

    delete

--- Data/Search.js
+++
@@ -1,1 +0,0 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/01_Application Quickstart Guide/01_1 Download the application.htm'],s:['+0'],p:[['+0',5],['own',5]]});
--- Resources/Scripts/require.config.js
+++
@@ -1,3 +0,0 @@
-require.config({
-    urlArgs: 't=637866880489973744'
-});
